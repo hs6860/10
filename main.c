08796122d52866a6c7ea0b0429ed8d493719cc25
hs6860 <jang_6860@sookmyung.ac.kr>
2020-11-05 16:25:55 +0900

    10-6

--- main.c
+++ main.c
@@ -5,22 +5,34 @@
 
 
 	
-	void swap(int *x, int *y){
-		int temp;
-		temp = *x;
-		*x=*y;
-		*y=temp;
+
+	
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int grade[5];
+	int sum=0;
+	int *ptr;
+	
+	for(i=0;i<5;i++){
+		
+		ptr = &grade[i];
+		printf("grade[%i] = ",i);
+		scanf("%d", ptr);
+		
 	}
 	
-	int main(int argc, char *argv[]) {
-		
-		int a=3;
-		int b=5;
-		
-		swap(&a,&b);
-		printf("a:%i, b:%i\n", a,b);
-		
-		return 0;
-	}
 
+	for(i=0;i<5;i++){
+		
+		ptr = &grade[i];
+		sum += *ptr;
+		printf("grade[%d] = %d\n",i,*ptr);
+	
+	}
+	
+	printf("average : %i\n", sum/5);
+	
+	return 0;
+}
 
